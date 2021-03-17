1af9bbdc3acb4a1a3f7d71f964fa544a5f7a0cde
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-17 15:48:04 +0600

    added h1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import "./App.css";
 import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        Dango-builder by Erjan Kydyrov
+        <h1>Dango-builder by Erjan Kydyrov</h1>
       </Layout>
     </div>
   );
