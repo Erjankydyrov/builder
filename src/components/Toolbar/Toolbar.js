c869389faca3737633b2a0d95d54f0929b75c3a4
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-19 16:00:37 +0600

    corected component Toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,13 +1,17 @@
+import DrawerOpen from "../Drawer/DrawerOpen/DrawerOpen";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
 import classes from "./Toolbar.module.css";
 
-const Toolbar = () => {
-    return ( 
-        <div className = {classes.Toolbar}>
-            <Logo />
+const Toolbar = ({ openDrawer }) => {
+    return (
+    <div className={classes.Toolbar}>
+        <Logo />
+        <nav>
             <Nav />
-        </div>
+        </nav>
+        <DrawerOpen click={openDrawer} />
+    </div>
     );
 }
  
