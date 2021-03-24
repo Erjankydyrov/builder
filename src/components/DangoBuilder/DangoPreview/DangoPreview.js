34812f2d4f153f36f204787f8564a9d2561a6d93
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-24 14:14:26 +0600

    added value DangoPreview

---
+++ src/components/DangoBuilder/DangoPreview/DangoPreview.js
@@ -0,0 +1,9 @@
+import classes from "./DangoPreview.module.css";
+
+const DangoPreview = () => {
+    return ( 
+        <div className = {classes.DangoPreview}></div>
+    );
+}
+ 
+export default DangoPreview;
