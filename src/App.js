dbbcc4d19e004ce24aa015162005d453a5243ae5
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-24 14:33:42 +0600

    added DangoIngredient

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Layout>
-        Dango-builder by Erjan Kydyrov
+        <h1>Dango-builder by Erjan Kydyrov</h1>
         <DangoBuilder />
       </Layout>
     </div>
---
+++ src/components/DangoBuilder/DangoIngredient/DangoIngredient.js
@@ -0,0 +1,9 @@
+import classes from "./DangoIngredient.module.css";
+
+const DangoIngredient = ({ type }) => {
+    return ( 
+        <div>{type}</div>
+    );
+}
+ 
+export default DangoIngredient;
