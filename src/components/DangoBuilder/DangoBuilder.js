e9aeaec88e6e9c10191bf7adafc2e6ea117c3b14
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-24 14:23:27 +0600

    added components in DangoBuilder

--- src/components/DangoBuilder/DangoBuilder.js
+++ src/components/DangoBuilder/DangoBuilder.js
@@ -1,8 +1,13 @@
 import classes from "./DangoBuilder.module.css";
+import DangoControls from "./DangoControls/DangoControls";
+import DangoPreview from "./DangoPreview/DangoPreview";
 
 const DangoBuilder = () => {
     return ( 
-        <div className = {classes.DangoBuilder}></div>
+        <div className = {classes.DangoBuilder}>
+            <DangoPreview />
+            <DangoControls />
+        </div>
     );
 }
  
