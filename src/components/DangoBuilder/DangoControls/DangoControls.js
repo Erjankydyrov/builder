c95a844009171abf11c696cdd7b912a9a3ee87fe
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-24 14:14:11 +0600

    added value DangoControls

---
+++ src/components/DangoBuilder/DangoControls/DangoControls.js
@@ -0,0 +1,9 @@
+import classes from "./DangoControls.module.css";
+
+const DangoControls = () => {
+    return ( 
+        <div className = {classes.DangoControls}></div>
+    );
+}
+ 
+export default DangoControls;
